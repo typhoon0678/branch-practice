b718be5197271a8c1af76e0a3eff17a56728be49
typhoon0678 <typhoon0678@gmail.com>
2024-03-13 19:14:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Branch Practice
+
+## 1차 목표
+- 3개의 브랜치에서 만든 기능 merge
+```
+1. Main 에서 LogIn, Score, Ranking 브랜치를 생성
+2. 기능을 각 브랜치에서 구현
+    - Login 브랜치
+        회원가입, 로그인, score 저장, 전체 테이블 전달 기능
+    - Score 브랜치
+        1ms마다 1씩 score가 오르는 기능
+    - Ranking 브랜치
+        순위 정렬 후, 3위까지 표시
+3. LogIn, Score, Ranking 순으로 Main에 적용
+```
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+
+        // ToDo: 로그인, 회원가입
+
+        // ToDo: 1ms 마다 1씩 증가
+
+        // ToDo: 높은 순위 검색
+
+    }
+}
